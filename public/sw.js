2780fbd601f33176a2413614dbdf1e77327574fc
DeepakisDemigod <deepakthapa1423@gmail.com>
2025-04-08 15:04:55 +0530

    change icon img dimension to 128×128 still not work

--- public/sw.js
+++ public/sw.js
@@ -1,11 +1,13 @@
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', () => self.clients.claim());
 
+const img = "pwa-128×128.png";
+
 self.addEventListener('push', event => {
   const data = event.data?.text() || 'Push notification';
   self.registration.showNotification('Push Message', {
     body: data,
-    icon: "./pwa-192x192.png",
+    icon: img,
     badge: "./pwa-72x72.png"
   });
 });
